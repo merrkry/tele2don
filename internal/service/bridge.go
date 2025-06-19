fb422d37d21a080dfbe1ab6ce2295fb11f6289cc
merrkry <merrkry@tsubasa.moe>
2025-06-19 13:57:35 +0200

    refactor: split HandleEndpointUpdates

--- internal/service/bridge.go
+++ internal/service/bridge.go
@@ -73,97 +73,19 @@ func (s *BridgeService) HandleEndpointUpdates(ctx context.Context, updatesChan <
 				continue
 			}
 
-			slog.Debug("Received update from endpoint", "id", update.UniqueEndpointMessageID, "rev", update.Timestamp, "content", update.Content)
-
-			// Query cache and update, obsolete or loopback messages are ignored.
-
-			var bid model.BridgeMessageID
-
-			time, err := s.Cache.QueryRevision(update.UniqueEndpointMessageID)
-			if err == nil {
-				if time.Equal(update.Timestamp) { // Already tracked message
-					continue
-				}
-				bid, err = s.Cache.QueryBridgeMessageID(update.UniqueEndpointMessageID)
-				if err != nil {
-					panic(fmt.Sprintf("Failed to query bridge message ID for %q: %v", update.UniqueEndpointMessageID, err))
-				}
-				err = s.Cache.UpdateEndpointMessage(update.UniqueEndpointMessageID, update.Timestamp)
-				if err != nil {
-					panic(fmt.Sprintf("Failed to update endpoint message for %q: %v", update.UniqueEndpointMessageID, err))
-				}
-			} else if errors.Is(err, ErrMessageNotFound) {
-				if update.Type == model.UpdateTypeNew {
-					bid = s.Cache.NewBridgeMessage()
-					err := s.Cache.CreateEndpointMessage(update.UniqueEndpointMessageID, bid, update.Timestamp)
-					if err != nil {
-						panic(fmt.Sprintf("Failed to create endpoint message for %q: %v", update.UniqueEndpointMessageID, err))
-					}
-				} else { // Message is older than our state, ignore it
-					continue
-				}
-			} else {
-				panic(fmt.Sprintf("Failed to query revision for %q: %v", update.UniqueEndpointMessageID, err))
+			bid, ok := s.queryOrCreateBridgeMessage(update)
+			if !ok {
+				continue
 			}
 
-			if bid == 0 {
-				panic(fmt.Sprintf("Failed to retrieve or generate bridge message ID for %q", update.UniqueEndpointMessageID))
-			}
-
-			slog.Debug("Processing update", "type", update.Type, "eid", update.UniqueEndpointMessageID.EID, "bid", bid, "rev", update.Timestamp)
-
-			// Forward update
+			slog.Debug("Processing endpoint update", "bid", bid, "uniqueID", update.UniqueEndpointMessageID, "type", update.Type, "timestamp", update.Timestamp)
 
 			switch update.Type {
 			case model.UpdateTypeNew:
-				for eid, endpoint := range s.Endpoints {
-					if endpoint.ID() == update.EID {
-						continue
-					}
-					eid := model.EndpointID(eid)
-
-					updateCtx, cancel := context.WithTimeout(ctx, s.Config.RequestTimeout)
-					defer cancel()
-
-					id, rev, err := endpoint.ApplyUpdateNew(updateCtx, update.Content)
-					if err != nil {
-						slog.Error("Failed to apply update to endpoint", "eid", endpoint.ID(), "err", err)
-						continue
-					}
-					err = s.Cache.CreateEndpointMessage(model.UniqueEndpointMessageID{
-						EID: eid,
-						ID:  id,
-					}, bid, rev)
-					if err != nil {
-						panic(fmt.Sprintf("Failed to create endpoint message for %q: %v", id, err))
-					}
-				}
+				s.applyUpdateNew(ctx, update, bid)
 
 			case model.UpdateTypeEdit:
-				associatedMessages, err := s.Cache.QueryEndpointMessages(bid)
-				if err != nil {
-					panic(fmt.Sprintf("Failed to query associated messages for bridge message ID %d: %v", bid, err))
-				}
-
-				for _, uniqueID := range associatedMessages {
-					if uniqueID.EID == update.UniqueEndpointMessageID.EID {
-						continue
-					}
-
-					ctx, cancel := context.WithTimeout(ctx, s.Config.RequestTimeout)
-					defer cancel()
-
-					rev, err := s.Endpoints[uniqueID.EID].ApplyUpdateEdit(ctx, uniqueID.ID, update.Content)
-					if err != nil {
-						slog.Error("Failed to apply update edit to endpoint", "eid", uniqueID.EID, "id", uniqueID.ID, "err", err)
-						continue
-					}
-
-					err = s.Cache.UpdateEndpointMessage(uniqueID, rev)
-					if err != nil {
-						panic(fmt.Sprintf("Failed to update endpoint message for %q: %v", uniqueID, err))
-					}
-				}
+				s.applyUpdateEdit(ctx, update, bid)
 
 			case model.UpdateTypeDelete:
 				slog.Error("Message deletion not supported yet.")
@@ -171,6 +93,98 @@ func (s *BridgeService) HandleEndpointUpdates(ctx context.Context, updatesChan <
 			default:
 				panic(fmt.Sprintf("Unknown update type %d for %q", update.Type, update.UniqueEndpointMessageID))
 			}
+		}
+	}
+}
+
+// queryOrCreateBridgeMessage queries the cache for an existing bridge message ID or creates a new one if it doesn't exist.
+// The second return value only indicates if the message should be processed further.
+// In case of invalid internal state, query/create will fail, it will panic.
+func (s *BridgeService) queryOrCreateBridgeMessage(update *model.EndpointUpdate) (model.BridgeMessageID, bool) {
+	var bid model.BridgeMessageID
+
+	time, err := s.Cache.QueryRevision(update.UniqueEndpointMessageID)
+	if err == nil {
+		if time.Equal(update.Timestamp) { // Already tracked message
+			return 0, false
+		}
+		bid, err = s.Cache.QueryBridgeMessageID(update.UniqueEndpointMessageID)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to query bridge message ID for %q: %v", update.UniqueEndpointMessageID, err))
+		}
+		err = s.Cache.UpdateEndpointMessage(update.UniqueEndpointMessageID, update.Timestamp)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to update endpoint message for %q: %v", update.UniqueEndpointMessageID, err))
+		}
+	} else if errors.Is(err, ErrMessageNotFound) {
+		if update.Type == model.UpdateTypeNew {
+			bid = s.Cache.NewBridgeMessage()
+			err := s.Cache.CreateEndpointMessage(update.UniqueEndpointMessageID, bid, update.Timestamp)
+			if err != nil {
+				panic(fmt.Sprintf("Failed to create endpoint message for %q: %v", update.UniqueEndpointMessageID, err))
+			}
+		} else { // Message is older than our state, ignore it
+			return 0, false
+		}
+	} else {
+		panic(fmt.Sprintf("Failed to query revision for %q: %v", update.UniqueEndpointMessageID, err))
+	}
+
+	if bid == 0 {
+		panic(fmt.Sprintf("Failed to retrieve or generate bridge message ID for %q", update.UniqueEndpointMessageID))
+	}
+
+	return bid, true
+}
+
+func (s *BridgeService) applyUpdateNew(ctx context.Context, update *model.EndpointUpdate, bid model.BridgeMessageID) {
+	for eid, endpoint := range s.Endpoints {
+		if endpoint.ID() == update.EID {
+			continue
+		}
+		eid := model.EndpointID(eid)
+
+		updateCtx, cancel := context.WithTimeout(ctx, s.Config.RequestTimeout)
+		defer cancel()
+
+		id, rev, err := endpoint.ApplyUpdateNew(updateCtx, update.Content)
+		if err != nil {
+			slog.Error("Failed to apply update to endpoint", "eid", endpoint.ID(), "err", err)
+			continue
+		}
+		err = s.Cache.CreateEndpointMessage(model.UniqueEndpointMessageID{
+			EID: eid,
+			ID:  id,
+		}, bid, rev)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to create endpoint message for %q: %v", id, err))
+		}
+	}
+}
+
+func (s *BridgeService) applyUpdateEdit(ctx context.Context, update *model.EndpointUpdate, bid model.BridgeMessageID) {
+	associatedMessages, err := s.Cache.QueryEndpointMessages(bid)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to query associated messages for bridge message ID %d: %v", bid, err))
+	}
+
+	for _, uniqueID := range associatedMessages {
+		if uniqueID.EID == update.UniqueEndpointMessageID.EID {
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(ctx, s.Config.RequestTimeout)
+		defer cancel()
+
+		rev, err := s.Endpoints[uniqueID.EID].ApplyUpdateEdit(ctx, uniqueID.ID, update.Content)
+		if err != nil {
+			slog.Error("Failed to apply update edit to endpoint", "eid", uniqueID.EID, "id", uniqueID.ID, "err", err)
+			continue
+		}
+
+		err = s.Cache.UpdateEndpointMessage(uniqueID, rev)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to update endpoint message for %q: %v", uniqueID, err))
 		}
 	}
 }
